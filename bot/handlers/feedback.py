df39a43e340b550918770a5dd38960384f4ffceb
маклаков даниил павлович <dpmaklakov@edu.hse.ru>
2026-06-27 00:33:19 +0300

    feature: apply code style standards, and add error handling and timing metrics for Prometheus.

--- bot/handlers/feedback.py
+++ bot/handlers/feedback.py
@@ -8,53 +8,75 @@ from aiogram.fsm.state import default_state
 
 from bot.config import config
 from bot.api.termeet import termeet_client
-
+from bot.metrics import get_feedback_latency
 
 if TYPE_CHECKING:
     from aiogram import Bot
+    from bot.api.schemas import Feedback
 
 router = Router()
 
 
 async def handler_order(data: dict, bot: "Bot"):
     text = (
-        f"💥Получен отзыв от {data["contact"]}💥\n"
-        f"Чтобы посмотреть используйте команду /feedback"
+        f"💥Получен отзыв от {data['contact']}💥\n"
+        f"Чтобы посмотреть используйте команду /feedback_json или /feedback_grpc"
     )
     for chat_id in config.telegram.CHAT_IDS_FOR_FEEDBACK:
         await bot.send_message(chat_id=chat_id, text=text)
 
 
 @router.message(Command("feedback_json"), StateFilter(default_state))
-async def get_all_feedback(message: Message):
-    start = time.perf_counter()
-    feedbacks = await termeet_client.get_all_feedback_json()
-    end = time.perf_counter()
-    print(f"Time: {end - start}", flush=True)
+async def get_all_feedback_json(message: Message):
 
-    # print(feedbacks, flush=True)
+    try:
+        start = time.perf_counter()
+        feedbacks: list[Feedback] = (
+            await termeet_client.get_all_feedback_json()
+        )
+        end = time.perf_counter()
 
-    # if not feedbacks:
-    #     await message.answer("Отзывов пока нет.")
-    #     return
+        get_feedback_latency.labels("json").observe(end - start)
 
-    # lines = []
+    except Exception:
+        await message.answer(
+            "Произошла ошибка при получении отзывов. Обратитесь к @d10110101 за помощью"
+        )
 
-    # for f in feedbacks:
-    #     lines.append(
-    #         f"📩 <b>{f['type']}</b> | {f['communication_channel']}: {f['contact']}\n"
-    #         f"{f['message']}"
-    #     )
+    lines = []
 
-    await message.answer("Ответ получен!")
+    for f in feedbacks:
+        lines.append(
+            f"📩 <b>{f.type.name}</b> | {f.communication_channel.name}: {f.contact}\n"
+            f"{f.message}"
+        )
+
+    await message.answer("\n".join(lines))
 
 
 @router.message(Command("feedback_grpc"), StateFilter(default_state))
 async def get_all_feedback_grpc(message: Message):
-    start = time.perf_counter()
-    feedbacks = await termeet_client.get_all_feedback_grpc()
-    end = time.perf_counter()
-    print(f"Time: {end - start}", flush=True)
-    print(f"Payload: {feedbacks.ByteSize()}", flush=True)
 
-    await message.answer("Ответ получен!")
+    try:
+        start = time.perf_counter()
+        feedbacks: list[Feedback] = (
+            await termeet_client.get_all_feedback_grpc()
+        )
+        end = time.perf_counter()
+
+        get_feedback_latency.labels("grpc").observe(end - start)
+
+    except Exception:
+        await message.answer(
+            "Произошла ошибка при получении отзывов. Обратитесь к @d10110101 за помощью"
+        )
+
+    lines = []
+
+    for f in feedbacks:
+        lines.append(
+            f"📩 <b>{f.type.name}</b> | {f.communication_channel.name}: {f.contact}\n"
+            f"{f.message}"
+        )
+
+    await message.answer("\n".join(lines))
